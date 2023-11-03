c813121c5443e6c40e49601d760982c95bfb26ad
burakdede0 <burak.dede.03@gmail.com>
2023-11-04 01:44:12 +0300

    daisy: Set target GApps arch
    Signed-off-by: burakdede0 <burak.dede.03@gmail.com>

--- aosp_daisy.mk
+++ aosp_daisy.mk
@@ -24,6 +24,7 @@ TARGET_USES_AOSP_RECOVERY := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := daisy
